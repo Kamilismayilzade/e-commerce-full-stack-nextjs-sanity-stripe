49213bed641b5f24c3ceccc713f435917e9162d3
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-04-17 21:59:57 +0200

    fetching data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { HeroBanner, Product, Cart, Navbar, Footer, FooterBanner, Layout } from '../../components'
+import { client } from '../../library/client'
+
 
 const Home = () => {
 
@@ -7,7 +9,7 @@ const Home = () => {
 
     <>
     
-      HeroBanner
+      <HeroBanner />
 
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
@@ -20,11 +22,25 @@ const Home = () => {
         ))}
       </div>
 
-      Footer
+      <FooterBanner />
 
     </>
 
   )
+}
+
+export async function getServerSideProps() {
+
+  const productQuery = '*[_type == "product"]';
+  const allProducts = await client.fetch(productQuery);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const allBanner = await client.fetch(bannerQuery);
+
+  return {
+    props: {allProducts, allBanner},
+  }
+
 }
 
 export default Home
