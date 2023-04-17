afa8e3d595477dea370b7a08bed71ee2fa7da007
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-04-17 23:26:50 +0200

    button and description rendering

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -17,13 +17,13 @@ const HeroBanner = ({ heroBanner }) => {
         <img src={urlForImage(heroBanner.image)} alt="laptops" className='hero-banner-image' />
 
         <div>
-          <Link href='/product/ID'>
-            <button type='button'>Button text</button>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type='button' style={{ background: ' #b7950b ', boxShadow: 'rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset', }} > {heroBanner.buttonText} </button>
           </Link>
 
           <div className='desc'>
             <h5>Description</h5>
-            <p>Description itself</p>
+            <p> {heroBanner.desc} </p>
           </div>
         </div>
 
