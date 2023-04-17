9520c5f0e1ea3b1868714e043fce2d194ac5161e
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-04-17 21:59:42 +0200

    Initial hero elements

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,10 +1,33 @@
+import Link from 'next/link'
 import React from 'react'
+
 
 const HeroBanner = () => {
 
   return (
 
-    <div>HeroBanner</div>
+    <div className='hero-banner-container'>
+
+      <div>
+
+        <p className='laptop-solo'>small text</p>
+        <h3>medium text</h3>
+        <img src="" alt="laptops" className='hero-banner-image' />
+
+        <div>
+          <Link href='/product/ID'>
+            <button type='button'>Button text</button>
+          </Link>
+
+          <div className='desc'>
+            <h5>Description</h5>
+            <p>Description itself</p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
     
   )
 }
