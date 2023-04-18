4c84edf18ef7673cbae3ef76615a496c925eda18
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-04-18 19:47:14 +0200

    Navbar component complete

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
+import {AiTwotoneShopping} from 'react-icons/ai';
 
 const Navbar = () => {
 
   return (
 
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      
+      <p className='logo'>
+        <Link href='/'>Tech Fashion</Link>
+      </p>
+
+      <button type='button' className='cart-icon'>
+        <AiTwotoneShopping />
+        <span className='cart-item-qty'>1</span>
+      </button>
+
+    </div>
     
   )
 }
