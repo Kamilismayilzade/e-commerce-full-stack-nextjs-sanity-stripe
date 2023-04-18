1278d84cdcaaccf7f89454a45fdb6959757c0520
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-04-18 16:24:24 +0200

    Initial footer banner render

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,10 +1,37 @@
+import Link from 'next/link'
 import React from 'react'
+import { urlForImage } from '../library/client'
 
-const FooterBanner = () => {
+const FooterBanner = ({ footerBanner: {discount, largeText1, largeText2, saleTime, smallText, midText, buttonText, image, product, desc} }) => {
 
   return (
 
-    <div>FooterBanner</div>
+    <div className='footer-banner-container'>
+
+      <div className='banner-desc'>
+
+        <div className='left'>
+          <p> {discount} </p>
+          <h3> {largeText1} </h3>
+          <h3> {largeText2} </h3>
+          <p> {saleTime} </p>
+        </div>
+
+        <div className='right'>
+          <p> {smallText} </p>
+          <h3> {midText} </h3>
+          <p> {desc} </p>
+
+          <Link href={`/product/${product}`}>
+            <button type='button'> {buttonText} </button>
+          </Link>
+        </div>
+
+        <img src={urlForImage(image)} className='footer-banner-image' alt="" />
+
+      </div>
+
+    </div>
     
   )
 }
