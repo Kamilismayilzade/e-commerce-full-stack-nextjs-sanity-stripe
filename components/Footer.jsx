eef788a43041e156c9873cd1e929a51be5bc0f06
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-04-19 16:25:19 +0200

    Footer component complete

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {BsPinterest} from 'react-icons/bs'
+import {RiInstagramFill} from 'react-icons/ri'
 
 const Footer = () => {
 
   return (
 
-    <div>Footer</div>
+    <div className='footer-container'>
+
+      <p className='icons'>
+        <BsPinterest /> <RiInstagramFill />
+      </p>
+      <p> &copy; Tech Fashion  2023 </p>
+
+    </div>
     
   )
 }
